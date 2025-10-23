6b12f234a6524813934fbbb8a8b7a6e8865a9672
grupoexnihilo <grupoexnihilo@gmail.com>
2025-10-23 11:06:14 -0300

    Fix(App.jsx): Ensuring correct React imports (useState, useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 // --- useEffect CORRIGIDO (Balanceamento de {} e ()) ---
   useEffect(() => {
     console.log("[APP useEffect onAuthStateChanged] INICIANDO LISTENER");
