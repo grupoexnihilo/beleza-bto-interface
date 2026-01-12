b5cac035b3cd79c6d2b360f618da9f9b6ef70c05
grupoexnihilo <grupoexnihilo@gmail.com>
2026-01-12 13:58:21 -0300

    Fix: Consertando o problema do login.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDCOC3CblgOB9Jqq22daVcv-czNrC5I3U0",
   authDomain: "belezabto3-0.firebaseapp.com",
@@ -5,8 +8,10 @@ const firebaseConfig = {
   storageBucket: "belezabto3-0.firebasestorage.app",
   messagingSenderId: "38059564151",
   appId: "1:38059564151:web:a3032707b30ee624f23a72"
-};import { initializeApp } from "firebase/app";
+};
 
 const app = initializeApp(firebaseConfig);
 
+// Exportamos o auth para o formulário de login conseguir usar
+export const auth = getAuth(app); 
 export default app;
